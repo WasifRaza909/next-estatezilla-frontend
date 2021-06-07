5079439e336cdabc9991a1e65fba15ab5b048ef8
unknown <wasifraza909@gmail.com>
2021-06-07 20:35:30 +0500

    Completed About page

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import styles from '@/styles/Header.module.css';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
+  const [user, setUser] = useState(false);
   return (
     <nav className={styles.navbar}>
       <div className={styles.navFlex}>
@@ -18,9 +20,16 @@ export default function Header() {
           <Link href='/account/dashboard'>
             <a className={styles.navLinksItems}>Dashboard</a>
           </Link>
-          <Link href='/dashboard'>
-            <a className={styles.navLinksItems}>Login</a>
-          </Link>
+
+          {!user ? (
+            <Link href='/account/login'>
+              <a className={styles.navLinksItems}>Login</a>
+            </Link>
+          ) : (
+            <Link href='/'>
+              <a className={styles.navLinksItems}>Logout</a>
+            </Link>
+          )}
         </ul>
       </div>
     </nav>
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
 import Layout from '@/components/Layout.js';
+import styles from '@/styles/about.module.css';
 
 export default function About() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <div className={styles.about}>
+        <h1>About This App</h1>
+        <p>This is an app to help in Buying & Selling of Properties</p>
+        <small>Version : 1.0.0</small>
+      </div>
     </Layout>
   );
 }
