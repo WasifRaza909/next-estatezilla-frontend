63e2002ff5ff2688e2f91060925bb84031b5727a
unknown <wasifraza909@gmail.com>
2021-06-08 15:39:23 +0500

    Created Login & Register Forms as well as changed some styling

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,5 +1,39 @@
 import Layout from '@/components/Layout';
+import styles from '@/styles/Login.module.css';
+import Link from 'next/link';
+import { FaUser } from 'react-icons/fa';
 
 export default function Login() {
-  return <Layout>Login</Layout>;
+  return (
+    <Layout>
+      <div className={styles.card}>
+        <h1>
+          <FaUser />
+          Log In
+        </h1>
+        <form className='form'>
+          <div className={styles.formControl}>
+            <label htmlFor='email'>Email Address</label>
+            <input type='email' name='email' id='email' placeholder='' />
+          </div>
+          <div className={styles.formControl}>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              placeholder=''
+            />
+          </div>
+          <input type='submit' value='Login' />
+        </form>
+        <p>
+          Don't have an account?
+          <Link href='/account/register'>
+            <a> Register</a>
+          </Link>
+        </p>
+      </div>
+    </Layout>
+  );
 }
--- pages/account/register.js
+++ pages/account/register.js
@@ -1,5 +1,52 @@
 import Layout from '@/components/Layout';
+import styles from '@/styles/Login.module.css';
+import Link from 'next/link';
+import { FaUser } from 'react-icons/fa';
 
 export default function Register() {
-  return <Layout>Register</Layout>;
+  return (
+    <Layout>
+      <div className={styles.card}>
+        <h1>
+          <FaUser />
+          Log In
+        </h1>
+        <form className='form'>
+          <div className={styles.formControl}>
+            <label htmlFor='username'>Username</label>
+            <input type='text' name='username' id='username' placeholder='' />
+          </div>
+          <div className={styles.formControl}>
+            <label htmlFor='email'>Email Address</label>
+            <input type='email' name='email' id='email' placeholder='' />
+          </div>
+          <div className={styles.formControl}>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              placeholder=''
+            />
+          </div>
+          <div className={styles.formControl}>
+            <label htmlFor='confirmPassword'>Confirm Password</label>
+            <input
+              type='password'
+              name='confirmPassword'
+              id='confirmPassword'
+              placeholder=''
+            />
+          </div>
+          <input type='submit' value='Register' />
+        </form>
+        <p>
+          Already have an account?
+          <Link href='/account/login'>
+            <a> Login</a>
+          </Link>
+        </p>
+      </div>
+    </Layout>
+  );
 }
