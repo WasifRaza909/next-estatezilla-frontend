f1e5dec5bdb169c445a0d10fae2ce5f59071d58d
unknown <wasifraza909@gmail.com>
2021-06-20 22:19:44 +0500

    Added phoneNumber to EstatePage

--- pages/estates/[id].js
+++ pages/estates/[id].js
@@ -36,9 +36,14 @@ export default function Estate({ est }) {
             </div>
             <h2 className={styles.price}>Rs {est.price}</h2>
           </div>
-          <p className={styles.address}>
-            <span>Address</span> : {est.estateAddress}
-          </p>
+          <div className={styles.bottomFlexTwo}>
+            <p className={styles.address}>
+              <span>Address</span> : {est.estateAddress}
+            </p>
+            <p>
+              <FaPhoneAlt /> {est.phoneNumber}
+            </p>
+          </div>
           <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1809.377915012897!2d67.06362665804616!3d24.906308996008065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjTCsDU0JzIyLjciTiA2N8KwMDMnNTMuMCJF!5e0!3m2!1sen!2s!4v1623317396291!5m2!1sen!2s'
             width='600'
